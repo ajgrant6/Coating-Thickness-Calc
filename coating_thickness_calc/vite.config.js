58a5b1eac64e64be4bfe60116949b70d19164d3f
AJ Grant <77135564+ajgrant6@users.noreply.github.com>
2024-09-16 20:15:21 -0700

    Set up github pages

--- coating_thickness_calc/vite.config.js
+++ coating_thickness_calc/vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Coating-Thickness-Calc",
   plugins: [
     vue(),
     vueDevTools(),
